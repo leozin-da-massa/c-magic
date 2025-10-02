1b2895f9b27cd7234223c5c342276ac5132e5f06
leozin-da-massa <leocoltrofranca@gmail.com>
2025-10-02 09:52:53 -0300

    Update terminalMTG.cpp

--- terminalMTG.cpp
+++ terminalMTG.cpp
@@ -2,11 +2,16 @@
 using namespace std; 
 class protected cartas () {
     public:
-
     private:
-      int vida ;
-      int atck ;
-      int defense;
+        int vida ;
+        int atck ;
+        int defense;   
+  
+    public:
+          ninjutso() {
+        
+    }
+        
       
 
   
